2959e97b03555c37128c51df9f23534b61f3ad6d
telinal <tlaiphangbam@gmail.com>
2022-01-01 17:16:31 +0530

    Splash fragment created

--- app/src/main/java/com/example/weathernow/SplashFragment.kt
+++ app/src/main/java/com/example/weathernow/SplashFragment.kt
@@ -1,21 +1,20 @@
 package com.example.weathernow
 
+import android.animation.Animator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.airbnb.lottie.LottieAnimationView
 import com.example.weathernow.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
 
     lateinit var binding: FragmentSplashBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    lateinit var lottie: LottieAnimationView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +22,28 @@ class SplashFragment : Fragment() {
     ): View? {
         binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lottieSplash.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToMainFragment())
+
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+
+            }
+        })
     }
 
 
